78842a784b39b0dbd99e2c2079114128e30192a2
Joyce-Wanjiku <39213308+Joyce-Wanjiku@users.noreply.github.com>
2018-06-02 12:00:43 +0300

    second commit

---
+++ index.py
@@ -0,0 +1,29 @@
+#  creating list that stores intergers
+list1 = [1, 2, 3, 4, 5 ];
+print("list1[1:5]:" , list1[1:5])
+
+# creating a store that stores string
+list2 = ["Joyce", "David", "Linnet"];
+print(" Value available at index 2 :"  , list2[2])
+
+# creating a store that stores  intergers and strings
+list3 = [1, 2, 3, "Charity", "Purity", "Agnes"];
+print("value available at index 1:" list3[1])
+
+# creating a tuple of your own choice 
+tup1 = ("turn", "look", "enjoy");
+print("tup1[1:2]" , tup1[1:2])
+
+# creatingg a set of your own choice
+set1 = {"apple", "banana", "cherry"};
+print(set1)
+
+# creatong a dictionary containing your details
+dict1 = (
+  "Name": "Joyce",
+  "Age": "20",
+  "Nationality": "Kenyan"
+  "Address": "Mukuru"
+  "Gender":"Female"
+);
+print(dict1)
